e27c6fbd9c851fd368c39eae76c515b4d3bbdc32
livianaindah <livianaindah2711@gmail.com>
2021-03-09 09:50:23 +0700

    first commit

--- src/CobaNaiveBayes2/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// mysqli_connect("localhost","root","") or die ("server tidak ditemukan");
-// mysql_select_db("ai_kredit") or die ("Database Tidak Ditemukan");
-
-$success = mysqli_connect("localhost","root","","nb_tennis")or die("gagal, database tidak ditemukan");
-
-?>
